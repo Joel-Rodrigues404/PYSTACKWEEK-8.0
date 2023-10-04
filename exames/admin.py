a4ca89ad6c5bd8afe08846e3be62e3ef585930d8
Joel-Rodrigues <joelsandersonrodrigues@gmail.com>
2023-10-04 08:55:01 -0300

    Aula 01 Finalizada

--- exames/admin.py
+++ exames/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import TiposExames, PedidosExames, SolicitacaoExame
 # Register your models here.
+
+
+admin.site.register(TiposExames)
+admin.site.register(PedidosExames)
+admin.site.register(SolicitacaoExame)
--- exames/models.py
+++ exames/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class TipoExames(models.Model):
-    
+class TiposExames(models.Model):
+    TIPO_CHOICES = (
+        ('I','Exame de imagem'),
+        ('S','Exame de Sangue')
+    )
     nome = models.CharField(max_length=50)
-    tipo = models.CharField(max_length=1)
+    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES)
     preco = models.FloatField()
     disponivel = models.BooleanField(default=True)
     horario_inicial = models.IntegerField()
@@ -13,3 +16,29 @@ class TipoExames(models.Model):
 
     def __str__(self) -> str:
         return self.nome
+    
+class SolicitacaoExame(models.Model):    
+    choice_status = (
+        ('E', 'Em análise'),        
+        ('F', 'Finalizado')
+    )    
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)    
+    exame = models.ForeignKey(TiposExames, on_delete=models.DO_NOTHING)    
+    status = models.CharField(max_length=2, choices=choice_status)  
+    # onde sera armazenado o pdf com o resultado do exame que sera salva em uma pasta resultados  
+    resultado = models.FileField(upload_to="resultados", null=True, blank=True)# Null e blank definem que n e nesseçario o resultado para salvar este modelo  
+    requer_senha = models.BooleanField(default=False)    
+    senha = models.CharField(max_length=16, null=True, blank=True)
+
+    def __str__(self):        
+        return f'{self.usuario} | {self.exame.nome}'
+    
+class PedidosExames(models.Model):    
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)    
+    #               de muitos pra muitos
+    exames = models.ManyToManyField(SolicitacaoExame)    
+    agendado = models.BooleanField(default=True)    
+    data = models.DateField()    
+    
+    def __str__(self):        
+        return f'{self.usuario} | {self.data}'
